11162c2c945e71255420f4c648af948eaa417be4
Álvaro <36482489+alvaroolt@users.noreply.github.com>
2019-05-21 12:36:02 +0200

    Delete ParametroNoNumericoException.java

--- examenSegundoTrimestreJava/Ejercicio1/negocio/excepciones/ParametroNoNumericoException.java
+++
@@ -1,9 +0,0 @@
-package examenSegundoTrimestreJava.Ejercicio1.negocio.excepciones;
-
-public class ParametroNoNumericoException extends Exception {
-
-	public ParametroNoNumericoException(String string) {
-
-		super(string);
-	}
-}
